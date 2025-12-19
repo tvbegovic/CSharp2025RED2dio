a2549352570cb92af8c13dff2262071884caceca
tvbegovic <tvrtko@adamant.hr>
2025-12-19 13:07:59 +0100

    gameshop početak

---
+++ GameShopWeb/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace GameShopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public GameController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet("genres")]
+        public List<Genre> GetGenres()
+        {
+            using (var connection = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                string sql = "SELECT * FROM Genre";
+                return connection.Query<Genre>(sql).ToList();
+            }
+       }
+    }
+}
---
+++ GameShopWeb/DomainClasses.cs
@@ -0,0 +1,14 @@
+﻿namespace GameShopWeb
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
